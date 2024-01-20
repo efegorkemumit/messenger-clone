19e7039d67d697ed0ddd3d2dec900b778854d2e3
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-01-20 23:03:52 +0300

    form 2

--- components/Inputs/Input.tsx
+++ components/Inputs/Input.tsx
@@ -16,7 +16,7 @@ const Input: React.FC<InputProps>= ({label, id, errors ,
     register ,disabled, required, type})=> {
   return (
     <div>
-
+ 
         <label>{label}</label>
         <input></input>
         
