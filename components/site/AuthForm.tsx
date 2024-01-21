77e16bbb1327927a3047e0e07902a31cb8896094
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-01-21 17:45:06 +0300

    variant

--- components/site/AuthForm.tsx
+++ components/site/AuthForm.tsx
@@ -1,7 +1,7 @@
 'use client';
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import Input from '../Inputs/Input';
-import { useForm , FieldValues } from 'react-hook-form';
+import { useForm , FieldValues, SubmitHandler } from 'react-hook-form';
 import Button from '../Buttons/Button';
 
 type Variant = 'LOGIN' | 'REGISTER';
@@ -21,6 +21,26 @@ function AuthForm() {
       password:''
     }
   });
+
+  const onSubmit : SubmitHandler<FieldValues> = (data) =>{
+
+    if(variant =="REGISTER"){
+      console.log("register")
+    }
+
+    if(variant =="LOGIN"){
+      console.log("register")
+    }
+  }
+
+  const toggleVariant = useCallback(()=>{
+    if(variant== 'LOGIN'){
+      SetVariant('REGISTER')
+    }else{
+      SetVariant('LOGIN');
+    }
+
+  },[variant])
   
   return (
     <div className='mt-10 mx-auto w-full max-w-md'>
@@ -28,14 +48,20 @@ function AuthForm() {
       <div className='bg-slate-100 shadow-xl rounded-xl px-10 py-6'>
 
         <form className='space-y-5'>
-          <Input id="name" 
-          label='Name' 
-          required
-          register={register}
-          errors={errors}
-          disabled={isLoading} 
-          type='text'
-           ></Input>
+
+          {variant === 'REGISTER' &&(
+
+              <Input id="name" 
+              label='Name' 
+              required
+              register={register}
+              errors={errors}
+              disabled={isLoading} 
+              type='text'
+              ></Input>
+
+          )}
+         
 
         <Input id="email" 
           label='Email Address' 
@@ -56,10 +82,37 @@ function AuthForm() {
            ></Input>
 
            <div>
-            <Button children="save"></Button>
+            <Button fullWidth type='submit' disabled={isLoading}>
+
+            {variant =='LOGIN' ? 'Sign in':'Register'}
+
+            </Button>
 
 
            </div>
+
+           <div className='mt-8'>
+
+            <div className='w-full border-t border-gray-500'></div>
+
+
+           </div>
+
+
+        <div className='flex gap-3 justify-center text-sm mt-8
+        text-gray-700'>
+           <div>
+            {variant =='LOGIN' ? 'New to messseger':'Already have a account'}
+           </div>
+
+           <div className='cursor-pointer underline' onClick={toggleVariant}>
+
+           {variant =='LOGIN' ? 'Create Account':'LOGIN'}
+
+
+           </div>
+
+        </div>
 
 
 
