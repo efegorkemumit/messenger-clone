2b0a1de4b48fa6aaa9cacab6d490eb8208490034
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-01-30 18:40:16 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true,
+        swcPlugins: [["next-superjson-plugin", {}]]
+      },
 
     images:{
         domains:[
